b02e496a36599a800630cfbbbc97a87cc61b4fae
MR.ERIK <80484346+ErikZF@users.noreply.github.com>
2026-06-04 18:40:46 -0600

    db: SP de detallePlanillaSemanal

---
+++ database/StoredProcedures/Planilla/spDetallePlanillaSemanal.sql
@@ -0,0 +1,62 @@
+CREATE OR ALTER PROCEDURE dbo.spDetallePlanillaSemanal
+    @inIdPlanillaSemanal INT
+    , @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+BEGIN TRY
+
+    SET @outResultCode = 0;
+
+    -- Detalle por dia, hora entrada, salida y movimientos generados
+    SELECT
+        MA.Fecha
+        , MA.HoraEntrada
+        , MA.HoraSalida
+        , TM.Nombre AS TipoMovimiento
+        , TM.Accion
+        , MP.Monto
+        , MP.SaldoBrutoAcum
+    FROM dbo.Comprobante AS C
+    INNER JOIN dbo.ComprobanteHora AS CH ON (CH.idComprobante = C.id)
+    INNER JOIN dbo.MarcaAsistencia AS MA ON (MA.id = CH.idMarcaAsistencia)
+    INNER JOIN dbo.MovPlanilla AS MP ON (MP.idComprobante = C.id)
+    INNER JOIN dbo.TipoMovimiento AS TM ON (TM.id = MP.idTipoMovimiento)
+    WHERE (C.idPlanillaSemanal = @inIdPlanillaSemanal)
+        AND (C.Tipo = 'H')
+    ORDER BY MA.Fecha, MA.HoraEntrada, TM.id;
+
+END TRY
+BEGIN CATCH
+
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+
+    DECLARE
+        @ErrMsg NVARCHAR(4000) = ERROR_MESSAGE()
+        , @ErrNum INT = ERROR_NUMBER()
+        , @ErrSev INT = ERROR_SEVERITY()
+        , @ErrStat INT = ERROR_STATE()
+        , @ErrLine INT = ERROR_LINE()
+        , @ErrProc NVARCHAR(128) = ISNULL(ERROR_PROCEDURE(), '')
+        , @outCode INT = 0
+        ;
+
+    SET @outResultCode = 50001;
+
+    EXEC dbo.spInsertarError
+        @InErrorNumber = @ErrNum
+        , @InErrorMessage = @ErrMsg
+        , @InErrorSeverity = @ErrSev
+        , @InErrorState = @ErrStat
+        , @InErrorLine = @ErrLine
+        , @InErrorProcedure = @ErrProc
+        , @outResultCode = @outCode OUTPUT
+        ;
+
+END CATCH;
+
+SET NOCOUNT OFF;
+END;
+GO
