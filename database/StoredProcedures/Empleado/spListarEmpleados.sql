ae945dd9150c187e952629fe1fefd57996ab7414
MR.ERIK <80484346+ErikZF@users.noreply.github.com>
2026-06-04 18:19:45 -0600

    db: SP de listarEmpleados

---
+++ database/StoredProcedures/Empleado/spListarEmpleados.sql
@@ -0,0 +1,65 @@
+CREATE OR ALTER PROCEDURE dbo.spListarEmpleados
+    @inFiltro VARCHAR(100)
+    , @outResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+BEGIN TRY
+
+    SET @outResultCode =0;
+
+    SELECT
+        E.id
+        , E.Nombre
+        , E.ValorDocumento
+        , E.CuentaBancaria
+        , E.FechaContratacion
+        , E.Activo
+        , P.Nombre AS NombrePuesto
+        , D.Nombre AS NombreDepartamento
+        , U.username
+        , U.Tipo
+    FROM dbo.Empleado AS E
+    INNER JOIN dbo.Puesto AS P ON (P.id = E.idPuesto)
+    INNER JOIN dbo.Departamento AS D ON (D.id = E.idDepartamento)
+    INNER JOIN dbo.Usuario AS U ON (U.id = E.idUsuario)
+    WHERE (E.Activo = 1)
+        AND (
+            (@inFiltro = '')
+            OR (E.Nombre LIKE '%' + @inFiltro + '%')
+        )
+    ORDER BY E.Nombre;
+
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+
+    DECLARE
+        @ErrMsg NVARCHAR(4000) = ERROR_MESSAGE()
+        , @ErrNum INT = ERROR_NUMBER()
+        , @ErrSev INT = ERROR_SEVERITY()
+        , @ErrStat INT = ERROR_STATE()
+        , @ErrLine INT = ERROR_LINE()
+        , @ErrProc NVARCHAR(128) = ISNULL(ERROR_PROCEDURE(), '')
+        , @outCode INT = 0
+        ;
+
+    SET @outResultCode = 50001;
+
+    EXEC dbo.spInsertarError
+        @InErrorNumber = @ErrNum
+        , @InErrorMessage = @ErrMsg
+        , @InErrorSeverity = @ErrSev
+        , @InErrorState = @ErrStat
+        , @InErrorLine = @ErrLine
+        , @InErrorProcedure = @ErrProc
+        , @outResultCode = @outCode OUTPUT
+        ;
+
+END CATCH;
+
+SET NOCOUNT OFF;
+END;
+GO  
